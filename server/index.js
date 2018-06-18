307c3477f85ba0bd4bc1b62238bcde57668ae169
Gary David Guzmán Muñoz <gary.2810.dav@gmail.com>
2018-06-18 19:36:15 -0400

    Resolved error npm >= 5.6.0

--- server/index.js
+++ server/index.js
@@ -1,25 +1,10 @@
-// import http from 'http'
-// import Debug from 'debug'
-// import app from './app'
-
-// const PORT = 3000
-// const debug = Debug('condor-backend:root')
-
-// app.listen(PORT, () => {
-//     debug(`Server running at port ${PORT}`)
-// })
 import http from 'http'
+import Debug from 'debug'
+import app from './app'
 
 const PORT = 3000
-
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write('Hola desde Café Condor')
-  res.end()
-})
+const debug = Debug('condor-backend:root')
 
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`)
+    debug(`Server running at port ${PORT}`)
 })
-
-// "start:server": "NODE_ENV=development DEBUG=condor-backend* nodemon server/index.js --exec babel-node"
