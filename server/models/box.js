c3e9d69da03477867619203ad5a0ed892e6df32c
garyDav <gary.2810.dav@gmail.com>
2018-11-12 15:23:50 -0400

    working box

--- server/models/box.js
+++ server/models/box.js
@@ -3,6 +3,12 @@ import uniqueValidator from 'mongoose-unique-validator'
 
 const BoxSchema = new Schema({
   day: { type: String, required: true, unique: true, index: true },
+  period: [
+    {
+      opening: { type: String, required: true },
+      closing: { type: String, required: false }
+    }
+  ],
   amount: { type: Number, required: true }
 })
 
